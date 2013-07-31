0e9f13a66e0a9f6cf022f224d13c141d4c45fe3c
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2013-07-31 14:43:13 -0400

    basic game functionality

--- src/main/java/org/ejeff/scrmbale/web/GameBacking.java
+++ src/main/java/org/ejeff/scrmbale/web/GameBacking.java
@@ -1,11 +1,15 @@
 package org.ejeff.scrmbale.web;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import org.ejeff.scrmbale.Game;
+import org.ejeff.scrmbale.Puzzle;
+import org.ejeff.scrmbale.Word;
 
 @ManagedBean(name = "game")
 @ViewScoped
@@ -14,15 +18,17 @@ public class GameBacking implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Game game;
+    private Puzzle currentPuzzle;
 
     private String wordInput;
 
     public GameBacking() {
         game = new Game();
-        game.startRound();
+        currentPuzzle = game.startRound();
     }
 
     public String submitWord() {
+        game.enterGuess(wordInput);
         return null;
     }
 
@@ -34,4 +40,20 @@ public class GameBacking implements Serializable {
         this.wordInput = wordInput;
     }
 
+    public List<String> getWords() {
+        List<String> words = new LinkedList<String>();
+        for (Word word : currentPuzzle.getWords()) {
+            if (word.isDiscovered()) {
+                words.add(word.getText());
+            } else {
+                String blanks = new String(new char[word.getText().length()]).replace("\0", "_ ");
+                words.add(blanks);
+            }
+        }
+        return words;
+    }
+
+    public int getScore() {
+        return game.getScore();
+    }
 }
