ebba551c458972c864c63e8b7de81c06a2738bb7
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2013-08-01 03:19:37 -0400

    randomize puzzle list

--- src/main/java/org/ejeff/scrmbale/FilePuzzleRepository.java
+++ src/main/java/org/ejeff/scrmbale/FilePuzzleRepository.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -44,6 +45,7 @@ public class FilePuzzleRepository implements PuzzleRepository {
             }
         }
 
+        Collections.shuffle(puzzles);
     }
 
     @Override
